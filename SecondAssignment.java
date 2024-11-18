3e51f1f0ae7b731e5d458c9d7e0a78e999daf60f
SalamMuhsein <salammuhsein13@gmail.com>
2024-11-18 18:23:02 +0800

    this is my 2nd Assignment

--- SecondAssignment.java
+++ SecondAssignment.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
-public class SecondAssignment {
-        private int Car1;
-        private int Car2;
-        private int Car3;
     
-    public SecondAssignment (String CarModel, String CarBrand, double CarPrice){
+class SecondAssignment{
+    public static void main(String[] args) {
     Scanner cars = new Scanner(System.in);
-    System.out.println("Car1: Proton X50, Price:?");
+    System.out.println("Car 1: Proton X50, Price:?");
     int car1 = cars.nextInt();
-    System.out.println("Car1: Proton X90, Price:?");
+    System.out.println("Car 2: Proton X90, Price:?");
     int car2 = cars.nextInt();
-    System.out.println("Car1: Proton X70, Price:?");
+    System.out.println("Car 3: Proton X70, Price:?");
     int car3 = cars.nextInt();
 
     System.out.println("Average car price :" + (car1+car2+car3)/3);
